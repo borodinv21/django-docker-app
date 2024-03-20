acc112cef8858aa1d340f63abca33dd8ae43c6a4
borodinv21 <vikc003@mail.ru>
2024-03-20 16:55:36 +0500

    add get_price func

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -13,7 +13,13 @@ class SubscriptionSerializer(serializers.ModelSerializer):
 
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        full_price = instance.service.full_price
+        return full_price - full_price * (instance.plan.discount_percent / 100)
+
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
